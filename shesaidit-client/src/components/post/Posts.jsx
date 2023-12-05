f6288dacc897cade879587f6bfaffb6cd5a64e14
hurinah <145384510+hurinah@users.noreply.github.com>
2023-12-04 22:14:07 -0500

    Add files via upload

--- shesaidit-client/src/components/post/Posts.jsx
+++ shesaidit-client/src/components/post/Posts.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { Post } from './Post';
 import { Filter } from '../Filter';
 import { useParams } from 'react-router-dom';
@@ -10,33 +9,40 @@ export const Posts = ({ isLoggedIn, setModal, sort, setSort }) => {
 
   useEffect(() => {
     fetchData();
-  }, [sort]);
+  }, [sort, sortType]);
 
   const fetchData = async () => {
-    await fetch(`https://blog-app-mern-85pk.onrender.com/posts/${sortType}`, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
+    try {
+      const response = await fetch(`https://us-east-1.aws.data.mongodb-api.com/app/shesaidit-abcvc/endpoint/posts/${sortType}`, {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error fetching data: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
   return (
-    <div className='sm:mx-5 '>
+    <div className='sm:mx-5'>
       <Filter sort={sort} setSort={setSort} sortType={sortType} />
-      {posts.map((post) => {
-        return (
-          <Post
-            key={post._id}
-            post={post}
-            fetchData={fetchData}
-            isLoggedIn={isLoggedIn}
-            setModal={setModal}
-          />
-        );
-      })}
+      {posts.map((post) => (
+        <Post
+          key={post._id.$oid} // Assuming _id is represented as {"$oid": "someId"}
+          post={post}
+          fetchData={fetchData}
+          isLoggedIn={isLoggedIn}
+          setModal={setModal}
+        />
+      ))}
     </div>
   );
 };
